3cf65f885187adf372a181fb33caf316006c764c
Arash Pirhayati <ArashPirhayati1381@gmail.com>
2024-08-13 18:58:39 +0330

    Completed

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# evm-transfer-bot
-EVM Transfer Bot can transfer random amount of gas token or native coin (between 0.00001 to 0.0001) of EVM blockchains (mainnet and testnet) automatically from a wallet that you want to other wallets (randomly interval between transfers)
+## EVM Transfer Bot
+
+EVM Transfer Bot can transfer random amount of gas token or native coin (between 0.00001 to 0.0001) of EVM blockchains (mainnet and testnet) automatically from a wallet that you want to other wallets (randomly interval between transfers 10s to 15s). I join many cryptocurrency airdrops and I usually use such bots that I code for projects that have testnet tasks (for example, the Superposition project recently).
+
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/0xArashz/evm-transfer-bot.git
+   ```
+
+2. Navigate into the project directory:
+
+   ```bash
+   cd evm-transfer-bot
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+4. Prepare input files:
+
+   - Create `.env` in the root and replace the `RPC_URL`, `CHAIN_ID` and `PRIVATE_KEY` values:
+
+   ```bash
+   RPC_URL=EVM_BLOCKCHAIN_RPC_URL
+   CHAIN_ID=EVM_BLOCKCHAIN_CHAIN_ID
+   PRIVATE_KEY=SENDER_WALLET_PRIVATE_KEY
+   ```
+
+   - Create `recipients.json` in the root with an array of recipients EVM addresses (as many as you want):
+
+   ```json
+   [
+     "RECIPENT_ADDRESS_01",
+     "RECIPENT_ADDRESS_02",
+     "RECIPENT_ADDRESS_03"
+   ]
+   ```
+
+
+### Usage
+
+Run bot:
+
+```bash
+npm start
+```
+
+<div>
+    <img src="https://github.com/user-attachments/assets/c5c068be-dbc9-4f8f-a181-87f50314926b" width="100%">
+</div>
---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const { Web3 } = require("web3");
+require("dotenv").config();
+
+
+const rpcURL = process.env.RPC_URL;
+const web3 = new Web3(rpcURL);
+const privateKey = process.env.PRIVATE_KEY;
+const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+web3.eth.accounts.wallet.add(account);
+web3.eth.defaultAccount = account.address;
+const recipients = JSON.parse(
+    fs.readFileSync("./recipients.json", "UTF-8")
+);
+
+const getRandomAmount = (min, max) => {
+    return (Math.random() * (max - min) + min).toFixed(8);
+};
+
+const getRandomDelay = () => {
+    return Math.floor(Math.random() * 6 + 10) * 1000;
+};
+
+let txCount = 0;
+const sendTransaction = async () => {
+    try {
+        for (let recipient of recipients) {
+            const amount = getRandomAmount(0.00001, 0.0001);
+            const txData = {
+                from: web3.eth.defaultAccount,
+                to: recipient,
+                value: web3.utils.toWei(amount, "ether"),
+                gas: 21000,
+                gasPrice: web3.utils.toWei("1", "gwei"),
+                chainId: process.env.CHAIN_ID
+            };
+            await web3.eth.sendTransaction(txData);
+            txCount++;
+            console.log(
+                "\x1b[32m%s\x1b[0m",
+                `--> #${txCount < 10 ? "0" + txCount : txCount} TRANSFER ${amount} COIN TO ${recipient} DONE \u2705`
+            );
+            if (txCount !== recipients.length) {
+                const interval = getRandomDelay();
+                setTimeout(sendTransaction, interval);
+            } else {
+                console.log("\n");
+                process.exit();
+            }
+        }
+    } catch (error) {
+        console.error("\x1b[31m%s\x1b[0m", "SOMETHING WENT WRONG!");
+    }
+};
+
+process.stdout.write("\x1Bc");
+console.log("\x1b[32m%s\x1b[0m", "##################################################");
+console.log("\x1b[32m%s\x1b[0m", "#                                                #");
+console.log("\x1b[32m%s\x1b[0m", "#             DEVELOPER: 0xArashz                #");
+console.log("\x1b[32m%s\x1b[0m", "#     GITHUB: https://github.com/0xArashz        #");
+console.log("\x1b[32m%s\x1b[0m", "#                                                #");
+console.log("\x1b[32m%s\x1b[0m", "##################################################\n\n");
+sendTransaction();
